64d11b232cc83ec3e5c2402368a827f716be2137
Peter Zeno <66540961+ZenoRobotics@users.noreply.github.com>
2024-09-04 13:11:57 -0600

    Update TB_Base.v

--- TB_Base.v
+++ TB_Base.v
@@ -1,39 +1,74 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/04/2024 12:02:33 PM
+// Design Name: 
+// Module Name: TB_Adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-module TB_Base();
+
+module TB_Adder();
+
+`timescale 1ns / 1ps
 
 // Instantiate DUT Module
 
 
 
 parameter c_CLOCK_PERIOD_NS = 10;  //100 MHz
-reg r_sys_clk = 0;
-reg r_rst_n = 1;
+reg sys_clk_r = 0;
+reg rst_n_r = 1;
 
-top_module Top_Module_Inst(
+reg [3:0] val_a_in_r = 0;
+reg [3:0] val_b_in_r = 0;
+wire [4:0] sum_c_out;
+
+add_module add_module_inst(
   //essential inputs
-  .clk(r_sys_clk),     //input wire 
-  .rst_n(r_rst_n),     //input wire 
-  //example outputs
-  .my_output()
+  .clk(sys_clk_r),     //input wire 
+  .rst(~rst_n_r),     //input wire 
+  //adder I/O
+  .a(val_a_in_r),     // 4 bits (15)
+  .b(val_b_in_r),     // 4 bits (15)
+  //sum output
+  .c(sum_c_out)       // 5 bits  
 );
 
   always
-    #(c_CLOCK_PERIOD_NS/2) r_sys_clk <= ~r_sys_clk;
+    #(c_CLOCK_PERIOD_NS/2) sys_clk_r <= ~sys_clk_r;
 
   initial
     begin
-       r_rst_n = 1;
+       rst_n_r = 1;
+       val_a_in_r = 1;
+       val_b_in_r = 2;
        #20
       repeat (5) begin
-            @(posedge r_sys_clk);
+            @(posedge sys_clk_r);
        end
-       r_rst_n = 0;
-       #35
-       r_rst_n = 1;
+       rst_n_r = 0;
+       #10
+       rst_n_r = 1;
+       @(posedge sys_clk_r);
+       val_a_in_r = 3;
+       val_b_in_r = 5;
+       
 
       #100;
+      $finish;
     end
-    
+
 endmodule
-       
